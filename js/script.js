97a9f3ba98c6ff0a78a3bd1f29770b9fde9075ed
StelsMaster <58134253+Stels26047@users.noreply.github.com>
2023-08-26 21:51:11 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function showSlide(){
     $('.x').show();
     $('.nav_menu_list').fadeIn();
     $('.hero_list-title').hide();
+    $('.nav_menu_button').hide();
     $('.bars').hide();
 }
 
@@ -9,6 +10,7 @@ function hideSlide(){
     $('.x').hide();
     $('.nav_menu_list').fadeOut();
     $('.hero_list-title').show();
+    $('.nav_menu_button').show();
     $('.bars').show();
 }
 
