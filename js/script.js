45f46f0bab363590700e5a04e0242313590dc3c6
StelsMaster <58134253+Stels26047@users.noreply.github.com>
2023-08-31 20:58:40 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 function showSlide(){
     $('.x').show();
     $('.nav_menu_list').fadeIn();
-    $('.nav_menu_button').hide();
     $('.bars').hide();
 }
 
 function hideSlide(){
     $('.x').hide();
     $('.nav_menu_list').fadeOut();
-    $('.nav_menu_button').show();
     $('.bars').show();
 }
 
